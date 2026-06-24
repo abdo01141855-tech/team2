4a41ef299a4351b2274568907fbb51a3ff193d6f
Nesrin elshawerby <nesrinelshawerby2005@gmail.com>
2026-06-24 16:35:19 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./Components/Hero";
+import Cards from "./Components/Cards";
+import About from "./Components/About";
+function App() {
+  return (
+    <>
+      
+      <Hero />
+      <Cards />
+      <About />
+    </>
+  );
+}
+
+export default App;
